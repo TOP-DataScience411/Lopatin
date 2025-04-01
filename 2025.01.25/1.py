dd65a82cd649ac6ee285e880c3ca3f20763b0b8c
Paul Lopatin <lopatinpavel96@gmail.com>
2025-04-01 13:30:49 +0300

    re compleated

--- 2025.01.25/1.py
+++ 2025.01.25/1.py
@@ -1,27 +1,20 @@
 import re
 from pathlib import Path
 
-month = (
-"январ(?:ь|я)|феврал(?:ь|я)|март?а|"
-"апрел(?:ь|я)|ма[йя]|июн(?:ь|я)|июл(?:ь|я)|"
-"август?а|сентябр(?:ь|я)|октябр(?:ь|я)|"
-"ноябр(?:ь|я)|декабр(?:ь|я)"
-)
+day = r"(?P<day>\d|(?:[12]\d|3[01]))"
+month = r"(?P<month>января|февраля|марта|апреля|мая|июня|июля|августа|сентября|октября|ноября|декабря)"
+year = r"(?P<year>\d{4})"
 
-year = r'^[12][089][0-9]{2} г\.'
-year_year = r'[12][089][0-9]{2}–[12][089][0-9]{2} гг\.'
-month_year = fr'^(?:{month}) [12][089][0-9]{{2}} г\.'
-day_month_year = rf'^(?:[1-9]\d?) (?:{month}) [12][089][0-9]{{2}} г\.'
-day_month_day_month_year = fr'^[1-9]\d? (?:{month}) – [1-9]\d? (?:{month}) [12][089][0-9]{{2}} г\.'
-day_month_year_day_month_year = fr'^[1-9]\d? (?:{month}) [12][089][0-9]{{2}} г\. – [1-9]\d? (?:{month}) [12][089][0-9]{{2}} г\.'
-day_day_month_year = fr'^[1-9]\d?–[1-9]\d? (?:{month}) [12][089][0-9]{{2}} г\.'
-month_month_year = fr'^(?:{month})–(?:{month}) [12][089][0-9]{{2}} г\.'
+#all = year+'|'+day+'|'+month
 
-pat = Path.cwd() / "history_dates_ed.txt"
+all = r'(?P<year>\d{4})|(?P<day>\d|(?:[12]\d|3[01]))|(?P<month>января|февраля|марта|апреля|мая|июня|июля|августа|сентября|октября|ноября|декабря)'
 
-with open(pat, encoding = 'utf-8') as file:
-  text = file.read()
+pattern = re.compile(all)
 
-result = re.findall(day_month_year_day_month_year, text, flags = re.MULTILINE)
-for res in result:
-    print(res)
+text = (Path.cwd()/"history_dates_ed.txt").read_text(encoding='utf-8')
+
+matches = pattern.findall(text,re.MULTILINE)
+
+print(matches)
+
+
